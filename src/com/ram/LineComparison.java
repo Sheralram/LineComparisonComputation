83b9462caf6a94997a59a13d5da458fcef474e37
Sheralram <ramkrishna.sheral@gmail.com>
2021-08-08 23:54:39 +0530

    Welcome to Line Comparison Computation

---
+++ src/com/ram/LineComparison.java
@@ -0,0 +1,8 @@
+package com.ram;
+
+public class LineComparison {
+    public static void main (String[] args)
+    {
+        System.out.println("Welcome to LineComparison Computation Program");
+    }
+}
